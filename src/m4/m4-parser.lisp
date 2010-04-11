ba118588f3ca30f9be4973e729a359f211ab0d0d
Alexander Kahl <e-user@fsfe.org>
2010-04-11 22:45:48 +0200

    new working, hand-written recursive parser. i kick ass

--- src/m4/m4-parser.lisp
+++ src/m4/m4-parser.lisp
@@ -15,7 +15,6 @@
 ;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (in-package :evol)
-(use-package '#:dso-parse)
 
 (define-condition macro-invocation-condition (error)
   ((result :initarg :result
@@ -32,23 +31,6 @@
 
 (defvar *m4-quoting-level*)
 (defvar *m4-string*)
-
-(defun m4-call-macro (macro args)
-  (format t "macro invocation ~a with args ~s~%" macro args)
-  (cond
-   ((string= "dnl" macro)
-    (progn
-      (let ((position (search (string #\newline) *m4-string*)))
-        (setq *m4-string*
-              (subseq *m4-string*
-                      (if position
-                          (1+ position)
-                        (length *m4-string*)))))
-      ""))
-    ((string= "format" macro)
-     (cerror "Use result of macro invocation" 'macro-invocation-condition :result "$")
-     "")
-    (t macro)))
                   
 (dso-lex:deflexer scan-m4 (:priority-only t)
   (" " :space)
@@ -59,44 +41,115 @@
   ("`" :quote-start)
   ("'" :quote-end)
   ("#" :comment)
-  ("dnl[^\\w]" :dnl)
   ("[_a-zA-Z]\\w+" :macro-name)
   ("[^ \\n(),`'#]" :token))
 
-(defgrammar ()
-  (m4 (* token))
-  (token (/ comment m4-string newline))
-  (comment (#\# (^ "[^\\n]*\\n")) :filter #'(lambda (s)
-                                           (format t "~a~%" s)))
-  (m4-string (^ "."))
-  (newline (^ "\\n")))
+(defun call-m4-macro (macro args)
+  (format t "macro invocation ~a with args ~s~%" macro args)
+  (cond
+   ((string= "format" macro)
+    (error 'macro-invocation-condition :result "$"))
+   (t (if args
+          (format nil "~a(~{~a~})" macro args)
+        macro))))
 
+(defun m4-lexer (&optional (peek nil))
+  (multiple-value-bind (class image remainder)
+      (scan-m4 *m4-string*)
+    (format t "read token [~a] of class [~a]~%" image class)
+    (when (and remainder (null peek))
+      (setq *m4-string* (subseq *m4-string* remainder)))
+    (values class image)))
 
+(defun parse-m4-comment (lexer image)
+  (labels ((m4-comment (rec)
+             (multiple-value-bind (class image)
+                 (funcall lexer)
+               (cond ((null class)
+                      (apply #'concatenate 'string (nreverse rec)))
+                     ((equal :newline class)
+                      (apply #'concatenate 'string
+                             (nreverse (cons image rec))))
+                 (t (m4-comment (cons image rec)))))))
+    (m4-comment (list image))))
 
+(defun parse-m4-quote (lexer)
+  (labels ((m4-quote (rec quoting-level)
+             (multiple-value-bind (class image)
+                 (funcall lexer)
+               (cond ((null class)
+                      (error "EOF with unfinished quoting")) ; TODO condition
+                     ((equal :quote-end class)
+                      (if (= 1 quoting-level)
+                          (apply #'concatenate 'string (nreverse rec))
+                        (m4-quote (cons image rec) (1- quoting-level))))
+                     ((equal :quote-start class)
+                      (m4-quote (cons image rec) (1+ quoting-level)))
+                     (t (m4-quote (cons image rec) quoting-level))))))
+    (m4-quote (list) 1)))
 
-  (file2 (+ row2))
-  (row2 (value (* row-rest2) (= newline))
-       :filter (lambda (row) (cons (car row) (mapcar #'second (second row)))))
-  (row-rest2 ((= comma) value) :filter 'identity)
-  (squoted-value ((= #\') (^ "([^']|'')+") (= #\'))
-                 :cclass t
-                 :filter
-                 (lambda (s) (cl-ppcre:regex-replace-all "''" (car s) "'")))
-  (dquoted-value ((= #\") (^ "([^\"]|\"\")+") (= #\"))
-                 :cclass t
-                 :filter
-                 (lambda (s) (cl-ppcre:regex-replace-all "\"\"" (car s) "\"")))
-  (unquoted-value (^ "[^,'\"\\n\\r]+") :cclass t)
-  (value (/ squoted-value dquoted-value unquoted-value) :cclass t)
-  (newline (^ "\\r\\n?|\\n"))
-  (comma #\,))
+(defun parse-m4-macro-arguments (lexer)
+  (labels ((m4-macro-arguments (rec paren-level)
+             (multiple-value-bind (class image)
+                 (funcall lexer)
+               (cond ((null class)
+                      (error "EOF with unfinished argument list")) ; TODO condition
+                     ((equal :close-paren class)
+                      (if (= 1 paren-level)
+                          (nreverse rec)
+                        (m4-macro-arguments (cons image rec) (1- paren-level))))
+                     ((equal :open-paren class)
+                      (m4-macro-arguments (cons image rec) (1+ paren-level)))
+                     ((equal :quote-start class)
+                      (m4-macro-arguments (cons (parse-m4-quote lexer) rec) paren-level))
+                     ((equal :comment class)
+                      (m4-macro-arguments (cons (parse-m4-comment lexer image) rec) paren-level))
+                     ((equal :macro-name class)
+                      (m4-macro-arguments (cons (parse-m4-macro lexer image) rec) paren-level))
+                     (t (m4-macro-arguments (cons image rec) paren-level))))))
+    (m4-macro-arguments (list "") 1)))
 
+(defun parse-m4-macro (lexer macro-name)
+  (handler-case
+   (multiple-value-bind (class image)
+       (funcall lexer t)
+     (declare (ignore image))
+     (cond ((string= "dnl" macro-name)
+            (when (equal :open-paren class)
+              (warn "excess arguments to builtin `dnl' ignored"))
+            (let ((position (search (string #\newline) *m4-string*)))
+              (setq *m4-string*
+                    (if position
+                        (subseq *m4-string* (1+ position))
+                      ""))
+              ""))
+           ((equal :open-paren class)
+            (funcall lexer) ; consume token
+            (call-m4-macro macro-name
+                           (parse-m4-macro-arguments lexer)))
+           (t (call-m4-macro macro-name nil))))
+   (macro-invocation-condition (condition)
+     (setq *m4-string* (concatenate 'string
+                                    (macro-invocation-result condition)
+                                    *m4-string*))
+     "")))
+
+(defun parse-m4 (lexer)
+  (labels ((m4 (rec)
+             (multiple-value-bind (class image)
+                 (funcall lexer)
+               (cond ((null class)
+                      (apply #'concatenate 'string (nreverse rec)))
+                     ((equal :quote-start class)
+                      (m4 (cons (parse-m4-quote lexer) rec)))
+                     ((equal :comment class)
+                      (m4 (cons (parse-m4-comment lexer image) rec)))
+                     ((equal :macro-name class)
+                      (m4 (cons (parse-m4-macro lexer image) rec)))
+                     (t (m4 (cons image rec)))))))
+    (m4 (list))))
 
 (defun test-m4 (string)
   (let ((*m4-quoting-level* 0)
         (*m4-string* (copy-seq string)))
-    (handler-bind ((macro-invocation-condition
-                    #'(lambda (error)
-                        (setq *m4-string* (concatenate 'string (macro-invocation-result error) *m4-string*))
-                        (invoke-restart 'continue))))
-                  (m4 *m4-string*))))
+    (parse-m4 #'m4-lexer)))
