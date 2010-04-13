01deb45e72c24a8f6200ac124d802759b411e2e9
Alexander Kahl <e-user@fsfe.org>
2010-04-13 17:40:23 +0200

    moved macro-invocation-result condition signalling where it belongs have `define' do variable substitution implemented `ifdef' and `ifelse' made *m4-lib* global again but create copy on-demand with with-m4-lib

--- src/m4/m4-builtin.lisp
+++ src/m4/m4-builtin.lisp
@@ -16,31 +16,71 @@
 
 (in-package :evol)
 
+(define-condition macro-invocation-condition (error)
+  ((result :initarg :result
+           :reader macro-invocation-result)))
+
 (define-condition macro-dnl-invocation-condition (error) ())
 
-(defvar *m4-lib*)
+(defparameter *m4-lib* (make-hash-table :test #'equal))
 
 (defmacro defm4macro (name args &body body)
   (let ((macro-args (gensym))
         (ignored-rest (gensym)))
     `(setf (gethash ,name *m4-lib*)
            #'(lambda (&rest ,macro-args)
-               (destructuring-bind (,@args &rest ,ignored-rest) ,macro-args
-                 (when ,ignored-rest
-                   (format *error-output* "excess arguments to builtin `~a' ignored~%" ,name))
-                 ,@body)))))
+               ,(if (member '&rest args)
+                    `(destructuring-bind ,args ,macro-args
+                       ,@body)
+                 `(destructuring-bind (,@args &rest ,ignored-rest) ,macro-args
+                    (when ,ignored-rest
+                      (warn (format nil "excess arguments to builtin `~a' ignored~%" ,name)))
+                    ,@body))))))
+
+(defun macro-return (result)
+  (error 'macro-invocation-condition :result result))
+
+(defm4macro "dnl" ()
+  (error 'macro-dnl-invocation-condition))
+
+(defm4macro "define" (name result)
+  (setf (gethash name *m4-lib*)
+        #'(lambda (&rest macro-args)
+            (cl-ppcre:regex-replace-all "\\$(\\d+)" result
+                                        (replace-with-region
+                                         #'(lambda (match)
+                                             (or (nth (1- (parse-integer match)) macro-args) ""))))))
+  "")
+
+(defm4macro "ifdef" (name string-1 &optional (string-2 ""))
+  (macro-return
+   (if (m4-macro name)
+       string-1
+     string-2)))
+
+(defm4macro "ifelse" (&rest args)
+  (labels ((ifelse (string-1 string-2 then &rest else)
+             (cond ((string= string-1 string-2)
+                    (macro-return then))
+                   ((> (list-length else) 1)
+                    (apply #'ifelse else))
+                   (t (macro-return (car else))))))
+    (let ((num-args (list-length args)))
+      (cond ((= 0 num-args) "ifelse") ; "The macro ifelse is recognized only with parameters"
+            ((= 1 num-args) "")       ; "Used with only one argument, the ifelse simply discards it and produces no output"
+            ((= 2 num-args)
+             (warn "too few arguments to builtin `ifelse'~%")
+             "")
+            ((< num-args 5)
+             (ifelse (car args) (cadr args) (caddr args) (or (cadddr args) "")))
+            ((= 5 num-args)
+             (warn "excess arguments to builtin `ifelse' ignored~%")
+             (ifelse (car args) (cadr args) (caddr args) (cadddr args)))
+            (t (apply #'ifelse (car args) (cadr args) (caddr args) (cdddr args)))))))
 
 (defun m4-macro (macro)
   (gethash macro *m4-lib*))
 
 (defmacro with-m4-lib (&body body)
-  `(let ((*m4-lib* (make-hash-table :test #'equal)))
-     (defm4macro "dnl" ()
-       (error 'macro-dnl-invocation-condition))
-     (defm4macro "define" (name result)
-       (setf (gethash name *m4-lib*)
-             #'(lambda (&rest macro-args)
-                 (declare (ignore macro-args))
-                   result))
-       "")
+  `(let ((*m4-lib* (alexandria:copy-hash-table *m4-lib*)))
      ,@body))
