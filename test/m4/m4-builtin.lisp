cf29d0430d9426566273a6f9219268e8a9750aa1
Alexander Kahl <e-user@fsfe.org>
2010-04-21 16:48:45 +0200

    m4 unit tests (finally!)

---
+++ test/m4/m4-builtin.lisp
@@ -0,0 +1,38 @@
+;;;; evol - m4-builtin.lisp
+;;;; Copyright (C) 2010  Alexander Kahl <e-user@fsfe.org>
+;;;; This file is part of evol.
+;;;; evol is free software; you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation; either version 3 of the License, or
+;;;; (at your option) any later version.
+;;;;
+;;;; evol is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(in-package :evol)
+
+(shadowing-import
+ '(m4-macro with-m4-lib macro-dnl-invocation-condition macro-defn-invocation-condition)
+ (find-package :evol-test))
+
+(in-package :evol-test)
+
+(in-suite m4)
+
+(defm4test macro-dnl-with-args "dnl" ("foo")
+  :signal macro-dnl-invocation-condition
+  :error (format nil "WARNING: excess arguments to builtin `dnl' ignored~%~%"))
+
+(defm4test macro-dnl-with-empty-args "dnl" (nil)
+  :signal macro-dnl-invocation-condition
+  :error (format nil "WARNING: excess arguments to builtin `dnl' ignored~%~%"))
+
+(defm4test macro-defn-no-args "defn" () :result "defn")
+
+(defm4test macro-defn-empty-args "defn" (nil)
+  :signal macro-defn-invocation-condition)
