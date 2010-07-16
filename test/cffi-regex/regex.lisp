91587d3737fb552a2a111817b5b0ff2d5eec16a0
Alexander Kahl <e-user@fsfe.org>
2010-07-16 17:55:16 +0200

    s/evol/cl-m4/g

--- test/cffi-regex/regex.lisp
+++ test/cffi-regex/regex.lisp
@@ -1,12 +1,12 @@
-;;;; evol - regex.lisp
+;;;; cl-m4 - regex.lisp
 ;;;; Copyright (C) 2010  Alexander Kahl <e-user@fsfe.org>
-;;;; This file is part of evol.
-;;;; evol is free software; you can redistribute it and/or modify
+;;;; This file is part of cl-m4.
+;;;; cl-m4 is free software; you can redistribute it and/or modify
 ;;;; it under the terms of the GNU General Public License as published by
 ;;;; the Free Software Foundation; either version 3 of the License, or
 ;;;; (at your option) any later version.
 ;;;;
-;;;; evol is distributed in the hope that it will be useful,
+;;;; cl-m4 is distributed in the hope that it will be useful,
 ;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;;;; GNU General Public License for more details.
@@ -14,7 +14,7 @@
 ;;;; You should have received a copy of the GNU General Public License
 ;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(in-package :evol-test)
+(in-package :cl-m4-test)
 
 (in-suite all)
 (defsuite cffi-regex)
