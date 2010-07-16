d48f1f58a28ac8b89d850a6e50b196edceb29d30
Alexander Kahl <e-user@fsfe.org>
2010-07-16 17:50:56 +0200

    updated copyrights

--- test/m4-macros.lisp
+++ test/m4-macros.lisp
@@ -1,6 +1,6 @@
-;;;; evol - m4-macros.lisp
+;;;; cl-m4 - m4-macros.lisp
 ;;;; Copyright (C) 2010  Alexander Kahl <e-user@fsfe.org>
-;;;; This file is part of evol.
+;;;; This file is part of cl-m4.
 ;;;; evol is free software; you can redistribute it and/or modify
 ;;;; it under the terms of the GNU General Public License as published by
 ;;;; the Free Software Foundation; either version 3 of the License, or
@@ -14,7 +14,7 @@
 ;;;; You should have received a copy of the GNU General Public License
 ;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(in-package :evol-test)
+(in-package :cl-m4-test)
 
 (in-suite m4)
 
